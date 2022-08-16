467dafe531931efd67363557491030a8c5eb5348
VS45 <bringfirehjake88@gmail.com>
2022-08-16 05:18:09 -0700

    make it better

--- components/DetailsBid.js
+++ components/DetailsBid.js
@@ -1,9 +1,34 @@
-import { View, Text } from "react-native";
-
-const DetailsBid = () => {
+import { View, Text, Image } from "react-native";
+import { EthPrice } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
+const DetailsBid = ({ bid }) => {
   return (
     <View>
-      <Text>DetailsBid</Text>
+      <Image
+        source={bid.image}
+        resizeMode="contain"
+        style={{ width: 48, height: 48 }}
+      />
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.small,
+          color: COLORS.primary,
+        }}
+      >
+        Bid placed by {bid.name}
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small - 2,
+          color: COLORS.primary,
+          marginTop: 3,
+        }}
+      >
+        {bid.date}
+      </Text>
+      <EthPrice price={bid.price} />
     </View>
   );
 };
--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -3,9 +3,27 @@ import { useState } from "react";
 import { EthPrice, NFTTitle } from "./SubInfo";
 import { COLORS, SIZES, FONTS } from "../constants";
 const DetailsDesc = ({ data }) => {
+  const [text, setText] = useState(data.description.slice(0, 100));
+  const [readMore, setReadMore] = useState(false);
+  function readMoreHandler() {
+    if (!readMore) {
+      setText(data.description);
+      setReadMore(true);
+    } else {
+      setText(data.description.slice(0, 100));
+      setReadMore(false);
+    }
+  }
   return (
     <>
-      <View>
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <NFTTitle
           title={data.name}
           subTitle={data.creator}
@@ -13,6 +31,44 @@ const DetailsDesc = ({ data }) => {
           subTitleSize={SIZES.font}
         />
         <EthPrice price={data.price} />
+      </View>
+      <View
+        style={{
+          marginVertical: SIZES.extraLarge * 1.5,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: SIZES.font,
+            fontFamily: FONTS.semiBold,
+            color: COLORS.primary,
+          }}
+        >
+          Description
+        </Text>
+        <View style={{ marginTop: SIZES.base }}>
+          <Text
+            style={{
+              fontSize: SIZES.small,
+              fontFamily: FONTS.regular,
+              color: COLORS.secondary,
+              lineHeight: SIZES.large,
+            }}
+          >
+            {text}
+            {!readMore && "..."}
+            <Text
+              style={{
+                fontSize: SIZES.small,
+                fontFamily: FONTS.semiBold,
+                color: COLORS.primary,
+              }}
+              onPress={readMoreHandler}
+            >
+              {readMore ? "Show Less" : "Read More"}
+            </Text>
+          </Text>
+        </View>
       </View>
     </>
   );
