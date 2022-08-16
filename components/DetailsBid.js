6d8e88d784f46ebcec284e53a12635a1cd50901b
VS45 <bringfirehjake88@gmail.com>
2022-08-16 05:52:14 -0700

    make it better

--- components/DetailsBid.js
+++ components/DetailsBid.js
@@ -5,12 +5,12 @@ const DetailsBid = ({ bid }) => {
   return (
     <View
       style={{
-        flexDirection: "row",
         width: "100%",
+        flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
         marginVertical: SIZES.base,
-        paddingHorizontal: 15,
+        paddingHorizontal: SIZES.base,
       }}
     >
       <Image
@@ -18,25 +18,27 @@ const DetailsBid = ({ bid }) => {
         resizeMode="contain"
         style={{ width: 48, height: 48 }}
       />
-      <Text
-        style={{
-          fontFamily: FONTS.semiBold,
-          fontSize: SIZES.small,
-          color: COLORS.primary,
-        }}
-      >
-        Bid placed by {bid.name}
-      </Text>
-      <Text
-        style={{
-          fontFamily: FONTS.regular,
-          fontSize: SIZES.small - 2,
-          color: COLORS.primary,
-          marginTop: 3,
-        }}
-      >
-        {bid.date}
-      </Text>
+      <View>
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.small,
+            color: COLORS.primary,
+          }}
+        >
+          Bid placed by {bid.name}
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small - 2,
+            color: COLORS.primary,
+            marginTop: 3,
+          }}
+        >
+          {bid.date}
+        </Text>
+      </View>
       <EthPrice price={bid.price} />
     </View>
   );
