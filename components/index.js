aaf24b4aff4988838c1630608810f96a941fef0d
VS45 <bringfirehjake88@gmail.com>
2022-08-15 17:11:38 -0700

    make it better

--- components/index.js
+++ components/index.js
@@ -1,6 +1,16 @@
 import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
 import { CircleButton, RectButton } from "./Button";
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton };
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+};
